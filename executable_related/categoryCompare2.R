1ee3f0c60c32240c19840e99414b1f4472a533dd
Robert M Flight <r.flight79@gmail.com>
2017-12-22 15:40:01 -0500

    everything now in a *main* function, and now writing the statistics to a file.

--- executable_related/categoryCompare2.R
+++ executable_related/categoryCompare2.R
@@ -34,13 +34,14 @@ Options:
 " -> doc
 
 default_config <- "genes: gene_file.json
+geneid-type: ENTREZID
 output-directory: cc2_results
 annotations: org.Hs.eg.db
 annotation-type: BP
 enrichment-test: hypergeometric
 enrichment-direction: over
 p-adjustment: BH
-p-cutoff: 0.01
+p-cutoff: 0.001
 count-cutoff: 2
 graph:
   min-edge-weight: 0.8
@@ -64,114 +65,150 @@ suppressMessages(library(categoryCompare2))
 
 script_options <- docopt(doc)
 
-if (script_options$`default-config`) {
-  cat(default_config, sep = "\n")
-}
-
-if (script_options$v | script_options$version) {
-  message(paste0("This is categoryCompare2 ", packageVersion("categoryCompare2")))
-}
-
-#print(script_options)
-
-#script_options <- script_options[!is.null(script_options)]
-
-print(script_options)
-
-# currently, this assumes that *all* the necessary options are set in the config.yml,
-# but eventually the goal would be it would just override defaults
-if (!(script_options$config %in% "NULL")) {
-  tmp_options <- yaml::read_yaml(file = script_options$config)
-  print(tmp_options)
-  script_options <- tmp_options
-  # add code for overriding the defaults
+main <- function(script_options){
+  
+  print(script_options)
+  
+  #browser(expr = TRUE)
+  if (!is.null(script_options$`default-config`)) {
+    if (script_options$`default-config`) {
+      cat(default_config, sep = "\n")
+      return()
+    }
+    
+  }
+  
+  
+  
+  if (!is.null(script_options$version)) {
+    if (script_options$version) {
+      message(paste0("This is categoryCompare2 ", packageVersion("categoryCompare2")))
+      return()
+    }
+    
+  }
+  
+  #print(script_options)
+  
+  #script_options <- script_options[!is.null(script_options)]
+  
+  
+  
+  # currently, this assumes that *all* the necessary options are set in the config.yml,
+  # but eventually the goal would be it would just override defaults
+  if (!is.null(script_options$config)) {
+    if (!(script_options$config %in% "NULL")) {
+      tmp_options <- yaml::read_yaml(file = script_options$config)
+      print(tmp_options)
+      script_options <- tmp_options
+    }
+    # add code for overriding the defaults
+  }
+  
+  # testing: script_options <- yaml::read_yaml(file = "cc2_config.yml")
+  
+  if (file.exists(script_options$genes)) {
+    gene_list <- jsonlite::fromJSON(script_options$genes)
+    if (is.null(gene_list$universe)) {
+      stop("The gene list does not have a universe / background!")
+    } else {
+      gene_universe <- gene_list$universe
+    }
+  } else {
+    stop("The gene list file ", script_options$genes, " does not exist. Make sure it exists on the search path!")
+  }
+  
+  go_types <- c("GO", "BP", "MF", "CC")
+  go_sub <- c("BP", "MF", "CC")
+  
+  # check if we are working with an organism db annotation
+  
+  if (grepl("eg.db", script_options$annotations)) {
+    if (!require(script_options$annotations, character.only = TRUE, quietly = TRUE)) {
+      stop("The package ", script_options$annotations, " is not installed/available. Try installing it with biocLite('", script_options$annotations, "')")
+    } else {
+      annotation_src <- eval(parse(text = script_options$annotations))
+      
+      if (script_options$`annotation-type` %in% go_types) {
+        suppressMessages(require("GO.db", character.only = TRUE))
+        gene_ann_map <- suppressMessages(AnnotationDbi::select(annotation_src, keys = gene_list$universe,
+                                                               keytype = script_options$`geneid-type`,
+                                                               columns = "GOALL"))
+        
+        if (script_options$`annotation-type` %in% go_sub) {
+          gene_ann_map <- gene_ann_map[gene_ann_map$ONTOLOGYALL %in% script_options$`annotation-type`, ]
+        }
+        
+        
+        ann_gene_list <- split(gene_ann_map[[script_options$`geneid-type`]], gene_ann_map[["GOALL"]])
+        ann_gene_list <- purrr::map(ann_gene_list, unique)
+        ann_description <- suppressMessages(AnnotationDbi::select(GO.db, keys = names(ann_gene_list), columns = "TERM", keytype = "GOID")$TERM)
+        names(ann_description) <- names(ann_gene_list)
+        
+        if (script_options$`annotation-type` %in% "GO") {
+          go_ontology_map <- unique(gene_ann_map[, c("GOALL", "ONTOLOGYALL")])
+          go_ontology <- go_ontology_map$ONTOLOGYALL
+          names(go_ontology) <- go_ontology_map$GOALL
+          go_ontology <- go_ontology[names(ann_description)]
+          ann_description <- paste0(go_ontology, ":", ann_description)
+        }
+        
+        
+        annotation_obj <- categoryCompare2::annotation(annotation_features = ann_gene_list,
+                                                       description = ann_description,
+                                                       type = script_options$`annotation-type`)
+      }
+    }
+  }
+  
+  # now that we are done making annotations, can remove the universe
+  gene_list$universe <- NULL
+  
+  gene_enrichments <- purrr::map(gene_list, function(in_genes){
+    hypergeometric_feature_enrichment(
+      new("hypergeom_features", significant = in_genes,
+          universe = gene_universe, annotation = annotation_obj),
+      p_adjust = script_options$`p-adjustment`
+    )
+  })
+  
+  combined_enrichments <- combine_enrichments(gene_enrichments)
+  
+  if (!(script_options$`p-adjustment` %in% "none")) {
+    p_cutoff_column <- "padjust"
+  } else {
+    p_cutoff_column <- "p"
+  }
+  
+  p_cutoff_value <- script_options$`p-cutoff`
+  p_cutoff_direction <- "<="
+  
+  count_cutoff_column <- "counts"
+  count_cutoff_value <- script_options$`count-cutoff`
+  count_cutoff_direction <- ">="
+  
+  count_call_info <- list(fun = count_cutoff_direction, var_1 = count_cutoff_column, var_2 = count_cutoff_value)
+  p_call_info <- list(fun = p_cutoff_direction, var_1 = p_cutoff_column, var_2 = p_cutoff_value)
+  
+  significant_calls <- list(counts = count_call_info, pvalues = p_call_info)
+  
+  combined_significant <- combined_significant_calls(combined_enrichments, significant_calls)
+  
+  results_table <- combined_significant@statistics@statistic_data
+  results_table$AnnotationID <- rownames(results_table)
+  results_table$annotationDescription <- combined_significant@annotation@description[results_table$AnnotationID]
+  
+  if (!dir.exists(script_options$`output-directory`)) {
+    dir.create(script_options$`output-directory`)
+  }
+  write.table(results_table, file = file.path(script_options$`output-directory`, "full_table.txt"),
+              sep = "\t", row.names = FALSE, col.names = TRUE)
+  
+  # next up will be code useful for handling wanting to make a graph
+  # significant_graph <- generate_annotation_graph(combined_significant)
+  # significant_graph <- remove_edges(significant_graph, script_options$graph$`min-edge-weight`)
+  
 }
 
 # testing: script_options <- yaml::read_yaml(file = "cc2_config.yml")
-
-if (file.exists(script_options$genes)) {
-  gene_list <- jsonlite::fromJSON(script_options$genes)
-  if (is.null(gene_list$universe)) {
-    stop("The gene list does not have a universe / background!")
-  } else {
-    gene_universe <- gene_list$universe
-  }
-} else {
-  stop("The gene list file ", script_options$genes, " does not exist. Make sure it exists on the search path!")
-}
-
-go_types <- c("GO", "BP", "MF", "CC")
-go_sub <- c("BP", "MF", "CC")
-
-# check if we are working with an organism db annotation
-if (grepl("eg.db", script_options$annotations)) {
-  if (!require(script_options$annotations, character.only = TRUE, quietly = TRUE)) {
-    stop("The package ", script_options$annotations, " is not installed/available. Try installing it with biocLite('", script_options$annotations, "')")
-  } else {
-    annotation_src <- eval(parse(text = script_options$annotations))
-    
-    if (script_options$`annotation-type` %in% go_types) {
-      suppressMessages(require("GO.db", character.only = TRUE))
-      gene_ann_map <- suppressMessages(AnnotationDbi::select(annotation_src, keys = gene_list$universe,
-                                            keytype = script_options$`gene-type`,
-                                            columns = "GOALL"))
-      
-      if (script_options$`annotation-type` %in% go_sub) {
-        gene_ann_map <- gene_ann_map[gene_ann_map$ONTOLOGYALL %in% script_options$`annotation-type`, ]
-      }
-      
-      
-      ann_gene_list <- split(gene_ann_map[[script_options$`gene-type`]], gene_ann_map[["GOALL"]])
-      ann_gene_list <- purrr::map(ann_gene_list, unique)
-      ann_description <- suppressMessages(AnnotationDbi::select(GO.db, keys = names(ann_gene_list), columns = "TERM", keytype = "GOID")$TERM)
-      names(ann_description) <- names(ann_gene_list)
-      
-      if (script_options$`annotation-type` %in% "GO") {
-        go_ontology_map <- unique(gene_ann_map[, c("GOALL", "ONTOLOGYALL")])
-        go_ontology <- go_ontology_map$ONTOLOGYALL
-        names(go_ontology) <- go_ontology_map$GOALL
-        go_ontology <- go_ontology[names(ann_description)]
-        ann_description <- paste0(go_ontology, ":", ann_description)
-      }
-      
-      
-      annotation_obj <- categoryCompare2::annotation(annotation_features = ann_gene_list,
-                                                     description = ann_description,
-                                                     type = script_options$`annotation-type`)
-    }
-  }
-}
-
-# now that we are done making annotations, can remove the universe
-gene_list$universe <- NULL
-
-gene_enrichments <- purrr::map(gene_list, function(in_genes){
-  hypergeometric_feature_enrichment(
-    new("hypergeom_features", significant = in_genes,
-        universe = gene_universe, annotation = annotation_obj),
-    p_adjust = script_options$`p-adjustment`
-  )
-})
-
-combined_enrichments <- combine_enrichments(gene_enrichments)
-
-if (!(script_options$`p-adjustment` %in% "none")) {
-  p_cutoff_column <- "padjust"
-} else {
-  p_cutoff_column <- "p"
-}
-
-p_cutoff_value <- script_options$`p-cutoff`
-p_cutoff_direction <- "<="
-
-count_cutoff_column <- "counts"
-count_cutoff_value <- script_options$`count-cutoff`
-count_cutoff_direction <- ">="
-
-count_call_info <- list(fun = count_cutoff_direction, var_1 = count_cutoff_column, var_2 = count_cutoff_value)
-p_call_info <- list(fun = p_cutoff_direction, var_1 = p_cutoff_column, var_2 = p_cutoff_value)
-
-significant_calls <- list(counts = count_call_info, pvalues = p_call_info)
-
-combined_enrichments <- combined_significant_calls(combined_enrichments, significant_calls)
-
+main(script_options)
