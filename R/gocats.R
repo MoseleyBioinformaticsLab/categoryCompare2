4ebcc762c8b02486e903384dc613e1a4b38b0739
rmflight <r.flight79@gmail.com>
2024-02-27 22:05:35 -0500

    makes a better gocats ancestors to annotation object

---
+++ R/gocats.R
@@ -0,0 +1,53 @@
+#' gocats to annnotations
+#' 
+#' Transforms a gocats ancestors JSON list to a GO annotation object.
+#' 
+#' @param ancestors_file the ancestors.json file from gocats (required)
+#' @param namespace_file the namespace.json file from gocats (optional)
+#' @param annotation_type what annotations are we making? (gocatsGO by default)
+#' @param feature_type what type of features are we using (assume Uniprot)
+#' 
+#' @return annotation object
+#' @export
+gocats_to_annotation = function(ancestors_file = "ancestors.json",
+                               namespace_file = "namespace.json",
+                               annotation_type = "gocatsGO",
+                               feature_type = "Uniprot")
+{
+  stopifnot(file.exists(ancestors_file))
+  
+  ancestors = jsonlite::fromJSON(ancestors_file)
+  go_2_gene = Biobase::reverseSplit(ancestors)
+  go_2_gene = purrr::map(go_2_gene, unique)
+  
+  if (!file.exists(namespace_file)) {
+    warning(paste0(namespace_file, " does not exist. GO namespace will not be updated."))
+    namespaces_short = character(0)
+  } else {
+    namespaces = jsonlite::fromJSON(namespace_file) |> unlist()
+    namespaces_short = gsub("biological_process", "BP", namespaces)
+    namespaces_short = gsub("molecular_function", "MF", namespaces_short)
+    namespaces_short = gsub("cellular_component", "CC", namespaces_short)
+  }
+  
+  if (requireNamespace("GO.db", quietly = TRUE)) {
+    descriptions = suppressMessages(AnnotationDbi::select(GO.db::GO.db, keys = names(go_2_gene), columns = "TERM", keytype = "GOID")$TERM)
+    names(descriptions) = names(go_2_gene)
+  } else {
+    message("GO.db is not installed, no descriptions will be added to the GO terms.")
+    descriptions = character(0)
+  }
+  
+  if ((length(namespaces_short) > 0) && (length(descriptions) > 0)) {
+    namespaces_short = namespaces_short[names(go_2_gene)]
+    descriptions = descriptions[names(go_2_gene)]
+    descriptions = paste0(namespaces_short, ":", descriptions)
+  }
+  
+  out_annotation = annotation(go_2_gene,
+                              annotation_type = annotation_type,
+                              description = descriptions,
+                              feature_type = feature_type)
+  return(out_annotation)
+  
+}
